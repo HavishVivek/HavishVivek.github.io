ae9d12f3547d656a33036a7de075c09a9679b011
sweetnessjourney <sweetnessjourney@gmail.com>
2026-01-03 14:38:05 -0600

    New features

--- app.py
+++ app.py
@@ -802,4 +802,4 @@ if __name__ == '__main__':
     print("🔗 Test it: http://localhost:5001/")
     print("=" * 70)
     
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5001)
