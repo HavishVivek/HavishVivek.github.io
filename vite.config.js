32c09dc05b1bbe2eee008ec36c3d2e07666994b1
HavishVivek <havish0624@gmail.com>
2026-01-29 22:06:34 -0600

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
-   import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue'
 
-    module.exports = defineConfig({
-      publicPath: process.env.NODE_ENV === 'production'
-       ? '/e-library/'
-       : '/'
-
-   export default defineConfig({
-     plugins: [vue()],
-     base: '/HavishVivek.github.io/',
-   })
+export default defineConfig({
+  plugins: [vue()],
+  base: '/HavishVivek.github.io/',
+})
